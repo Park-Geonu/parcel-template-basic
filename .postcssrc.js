3a0304848a25bfb936309cfc177e4ef64a53fe3b
박건우 <park-geonu@baggeon-us-MacBook-Air.local>
2023-07-03 02:20:19 +0900

    parcel project start

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM
+// Common JS
+
+// const autoprefixer = require('autoprefixer')
+
+// module.exports = {
+//   Plugins: [
+//     autoprefixer
+//   ]
+// }
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
